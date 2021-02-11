9ae10281a4cdaa4d5032e04d619c62b12214c7af
kevall474 <kevall474@tuta.io>
2021-02-10 22:55:03 -0500

    Create gcc-toolchain-to-git.sh

---
+++ gcc-toolchain-to-git.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+
+
+#_                   _ _ _  _ _____ _  _
+#| | _______   ____ _| | | || |___  | || |
+#| |/ / _ \ \ / / _` | | | || |_ / /| || |_
+#|   <  __/\ V / (_| | | |__   _/ / |__   _|
+#|_|\_\___| \_/ \__,_|_|_|  |_|/_/     |_|
+
+git clone https://github.com/kevall474/linux-api-headers-git && cd linux-api-headers-git && makepkg -si && cd ..
+
+git clone https://github.com/kevall474/glibc-git && cd glibc-git && makepkg -si && cd ..
+
+git clone https://github.com/kevall474/binutils-git && cd binutils-git && makepkg -si && cd ..
+
+git clone https://github.com/kevall474/gcc-git && cd gcc-git && makepkg -si && cd ..
