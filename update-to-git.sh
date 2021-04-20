1da597b2d806c064fa2e28d5b06f3bf6fb146d07
kevall474 <kevall474@tuta.io>
2021-04-20 19:29:49 -0400

    Add files via upload

--- update-to-git.sh
+++ update-to-git.sh
@@ -1,3 +1,67 @@
+#!/usr/bin/bash
+
+# build llvm (git version)
+git clone https://github.com/kevall474/llvm-git.git
+cd llvm-git
+###
+# my personnal command (for my machine)
+#sed -i '92s/ninja/ninja -j12/' clang-git/PKGBUILD
+#sed -i 's/ninja -C build all/ninja -j12 -C build all/' lib32-llvm-git/PKGBUILD
+##
+cd llvm-git && makepkg -si --noconfirm && cd ..
+cd lld-git && makepkg -si --noconfirm && cd ..
+cd openmp-git && makepkg -si --noconfirm && cd ..
+cd polly-git && makepkg -si --noconfirm && cd ..
+cd compiler-rt-git && makepkg -si --noconfirm && cd ..
+cd clang-git && makepkg -si --noconfirm && cd ..
+cd lldb-git && makepkg -si --noconfirm && cd ..
+cd spirv-llvm-translator-git && makepkg -si -- noconfirm && cd ..
+cd libclc-git && makepkg -si --noconfirm && cd ..
+#cd libc++-git && makepkg -si --noconfirm && cd ..
+cd lib32-llvm-git && makepkg -si --noconfirm && cd ..
+# make a copy of every pkg in package-$(date -I)/ dir
+mkdir -p package-$(date -I)
+cp -v */*.pkg.tar.zst package-$(date -I)/
+# clean build dir
+rm -rf */src/
+rm -rf */pkg/
+
+# build robin-hood-hashing
+git clone https://github.com/kevall474/Package.git
+cd Package
+cd robin-hood-hashing-git && makepkg -si --noconfirm && cd ..
+# make a copy of every pkg in package-$(date -I)/ dir
+mkdir -p package-$(date -I)
+cp -v */*.pkg.tar.zst package-$(date -I)/
+# clean build dir
+rm -rf */src/
+rm -rf */pkg/
+cd ..
+
+# build KhronosGroup package exept ocl-icd that depend on Mesa
+git clone https://github.com/kevall474/KhronosGroup.git
+cd KhronosGroup
+chmod +x build.sh
+./build.sh
+cd ..
+
+# build Xorg packages
+git clone https://github.com/kevall474/Xorg.git
+cd Xorg
+chmod +x build.sh
+./build.sh
+cd ..
+
+# build libdrm
+git clone https://github.com/kevall474/DRM.git
+cd DRM
+chmod +x build.sh
+./build.sh
+cd ..
+
+# build libglvnd
+git clone https://github.com/kevall474/GLVND.git
+cd GLVND
 chmod +x build.sh
 ./build.sh
 cd ..
