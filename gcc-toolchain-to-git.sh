0520b96be26a8e3170b75fef7ac567e10172d854
kevall474 <kevall474@tuta.io>
2021-04-30 13:27:35 -0400

    Update gcc-toolchain-to-git.sh

--- gcc-toolchain-to-git.sh
+++ gcc-toolchain-to-git.sh
@@ -6,7 +6,7 @@
 #|   <  __/\ V / (_| | | |__   _/ / |__   _|
 #|_|\_\___| \_/ \__,_|_|_|  |_|/_/     |_|
 
-git clone https://github.com/kevall474/glibc-git && cd glibc-git && makepkg -si && cd ..
+git clone https://github.com/kevall474/glibc-git && cd glibc-git && glibc-git && makepkg -si && cd .. && cd ..
 
 git clone https://github.com/kevall474/binutils-git && cd binutils-git && makepkg -si && cd ..
 
@@ -14,4 +14,4 @@ git clone https://github.com/kevall474/gcc-git && cd gcc-git && makepkg -si && c
 
 cd binutils-git && rm -rf *.pkg.tar.zst && rm -rf pkg src && makepkg -si && cd ..
 
-cd glibc-git && rm -rf *.pkg.tar.zst && rm -rf pkg src && makepkg -si && cd ..
+cd glibc-git && glibc-git && rm -rf *.pkg.tar.zst && rm -rf pkg src && makepkg -si && cd .. && cd ..
